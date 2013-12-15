c8fd1fd1eb6f6b39594a9c41113ba8f40ab850b6
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2013-12-15 17:36:53 +0200

    init + base bluetooth

---
+++ App/src/main/java/io/github/mabdrabo/sccop/MainActivity.java
@@ -0,0 +1,26 @@
+package io.github.mabdrabo.sccop;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent().setClass(this, Bluetooth.class);
+        startActivity(intent);
+    }
+
+}
