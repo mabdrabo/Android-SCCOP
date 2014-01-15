adbe4ecb3fb90bbd752c05c4b00d890fbf62c3f6
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-15 20:40:45 +0200

    color alerts

--- App/src/main/java/io/github/mabdrabo/sccop/GridItemAdapter.java
+++ App/src/main/java/io/github/mabdrabo/sccop/GridItemAdapter.java
@@ -1,6 +1,7 @@
 package io.github.mabdrabo.sccop;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,8 +53,56 @@ public class GridItemAdapter extends BaseAdapter {
             gridView = inflater.inflate(R.layout.grid_item, null);
             ((TextView) gridView
                     .findViewById(R.id.nameTextView)).setText(this.names[position]);
-            ((TextView) gridView
-                    .findViewById(R.id.valueTextView)).setText(this.values[position]);
+
+            TextView valueTextView= (TextView) gridView.findViewById(R.id.valueTextView);
+            if (this.values[position] != null) {
+                int warning = 0, danger = 0;
+                switch (position) {
+                    case 0:
+                        warning = 2700;
+                        danger = 4500;
+                        break;
+                    case 1:
+                        warning = 80;
+                        danger = 120;
+                        break;
+                    case 2:
+                        warning = 9;
+                        danger = 25;
+                        break;
+                    case 3:
+                    case 5:
+                        warning = 50;
+                        danger = 70;
+                        break;
+                    case 4:
+                        warning = 50;
+                        danger = 30;
+                }
+                if (position == 2) {
+                    if (Integer.parseInt(this.values[position]) > danger)
+                        valueTextView.setTextColor(Color.RED);
+                    else if (Integer.parseInt(this.values[position]) < warning)
+                        valueTextView.setTextColor(Color.YELLOW);
+                    else
+                        valueTextView.setTextColor(Color.GREEN);
+                } else if (position == 4) {
+                    if (Integer.parseInt(this.values[position]) < danger)
+                        valueTextView.setTextColor(Color.RED);
+                    else if (Integer.parseInt(this.values[position]) < warning)
+                        valueTextView.setTextColor(Color.YELLOW);
+                    else
+                        valueTextView.setTextColor(Color.GREEN);
+                } else {
+                    if (Integer.parseInt(this.values[position]) > danger)
+                        valueTextView.setTextColor(Color.RED);
+                    else if (Integer.parseInt(this.values[position]) > warning)
+                        valueTextView.setTextColor(Color.YELLOW);
+                    else
+                        valueTextView.setTextColor(Color.GREEN);
+                }
+            }
+            valueTextView.setText(this.values[position]);
             ((TextView) gridView
                     .findViewById(R.id.unitTextView)).setText(this.units[position]);
         } else {
